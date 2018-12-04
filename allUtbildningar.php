26776e573d5416a154752b6e50a94163111700df
Andreas Sanz Lerena <94andsan@mk.se>
2018-12-04 10:24:10 +0100

    Ändrade alla utbildningarsidan till php

---
+++ allUtbildningar.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Utbildningar - Campus Mölndal</title>
+</head>
+<body>
+<?php include_once 'menu.php' ?>
+<main>
+    <section id="headerArea">
+        <h1>Utbildningar</h1>
+    </section>
+    <div class="mainWithoutSidebarFull">
+        <div class="educations-grid" id="educations">
+
+        </div>
+    </div>
+</main>
+<?php include_once 'footer.php' ?>
+<script src="js/getUtbildningar.js"></script>
+<script src="js/javascript.js"></script>
+</body>
+</html>
