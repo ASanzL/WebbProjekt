9b59d5aa2019fa39a16e920b8b8db87d8fa7a3a9
Linus Orrekvist <linus.orrekvist97@gmail.com>
2018-12-05 11:23:43 +0100

    php

--- javaochwebb.php
+++ javaochwebb.php
@@ -99,37 +99,7 @@
         </section>
     </section>
 </main>
-<footer>
-    <section class="footerGrid">
-        <article class="footerItem">
-            <h2>Campus Mölndal</h2>
-            <p>Vägledningsexpedition</p>
-            <p>- för dig som vill studera</p>
-            <p>031-315 10 14</p>
-            <p>vagledning.campus@molndal.se</p>
-        </article>
-        <article class="footerItem">
-            <h2>Campus Mölndal</h2>
-            <p>Skolexpedition</p>
-            <p>- för dig som är elev</p>
-            <p>031-315 31 30</p>
-            <p>skolexp.campus@molndal.se</p>
-        </article>
-        <article class="footerItem">
-            <h2>Snabblänkar</h2>
-            <a href="#">Ansökan</a>
-            <a href="contact.html">Kontakt</a>
-            <a href="#">Itslearning</a>
-            <a href="#">Synpunkter</a>
-            <a href="#">Felanmälan</a>
-        </article>
-        <article class="footerItemSocial">
-            <h2>Social media</h2>
-            <a href="https://www.facebook.com"><img src="img/facebook.png"></a>
-        </article>
-    </section>
-    <p>Copyright © Campus Mölndal</p>
-</footer>
+<?php include_once 'footer.php' ?>
 <script src="js/javascript.js"></script>
 </body>
 </html>
